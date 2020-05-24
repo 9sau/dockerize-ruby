fc0060bbf8b26a8bce84ceaa97e836c4e6e6c600
Saurabh Jain <saurabhjain.kjsce@gmail.com>
2020-05-23 23:37:53 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/9sau/dockerize-ruby" }
+
+# Specify your gem's dependencies in dockerize-ruby.gemspec
+gemspec
---
+++ spec/dockerize/ruby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Dockerize::Ruby do
+  it "has a version number" do
+    expect(Dockerize::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
